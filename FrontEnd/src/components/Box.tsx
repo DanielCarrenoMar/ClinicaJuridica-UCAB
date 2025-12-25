70bacea54689e6e1c0fc3186a48df3320ed890bd
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2025-12-25 10:22:32 -0400

    feat: acomodar color de Box

--- FrontEnd/src/components/Box.tsx
+++ FrontEnd/src/components/Box.tsx
@@ -2,7 +2,7 @@
 function Box({ className = '', children, ...props }: React.HTMLAttributes<HTMLDivElement>) {
     
     return ( 
-        <div className={`p-4 bg-surface/70 rounded-xl ${className}`} {...props}>
+        <div className={`p-4 bg-surface/40 rounded-xl ${className}`} {...props}>
             {children}
         </div>
     );
