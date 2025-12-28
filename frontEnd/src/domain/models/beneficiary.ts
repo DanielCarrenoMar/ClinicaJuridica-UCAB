75f472203e48869f180d541b63ce8f4987cdf46c
Bloodbay8 <153459461+Bloodbay8@users.noreply.github.com>
2025-12-28 13:46:05 -0400

    feat: Models de Beneficiario y Solicitante

---
+++ frontEnd/src/domain/models/beneficiary.ts
@@ -0,0 +1,19 @@
+import type { PersonID, SexType, IdNacionality } from "#domain/mtypes.ts";
+
+type BeneficiaryType = "BENEFICIARY" | "APPLICANT";
+
+export interface BeneficiaryModel {
+    identityCard: PersonID;
+    gender: SexType;
+    birthDate: Date;
+    name: string;
+    idType: IdNacionality;
+    hasId: boolean;
+    type: BeneficiaryType;
+    idState?: number;
+    stateName?: string;
+    municipalityNumber?: number;
+    municipalityName?: string;
+    parishNumber?: number;
+    parishName?: string;
+}
