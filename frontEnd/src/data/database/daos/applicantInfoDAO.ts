6782a0dfd56981aebdf758e278aabc575877bcd8
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-12-30 14:45:03 -0400

    feat: info y normal para solicitante y beneficiario

---
+++ frontEnd/src/data/database/daos/applicantInfoDAO.ts
@@ -0,0 +1,7 @@
+import type { ApplicantDAO } from "./applicantDAO.ts";
+
+export interface ApplicantInfoDAO extends ApplicantDAO {
+	headEducationLevelName?: string;
+	workConditionName?: string;
+	activityConditionName?: string;
+}
