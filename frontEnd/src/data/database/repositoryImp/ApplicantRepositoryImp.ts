cc5bfab4099bb5bb7ce0c763bf39454f3967b4ec
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-12-30 12:21:03 -0400

    feat: tomar en cuenta posibles datos en null para solicitante

--- frontEnd/src/data/database/repositoryImp/ApplicantRepositoryImp.ts
+++ frontEnd/src/data/database/repositoryImp/ApplicantRepositoryImp.ts
@@ -1,6 +1,6 @@
 import type { ApplicantRepository } from "../../../domain/repositories";
 import type { ApplicantDAO } from "../daos/applicantDAO";
-import { daoToApplicantModel } from "#domain/models/applicant.ts";
+import { daoToApplicantModel, type ApplicantModel } from "#domain/models/applicant.ts";
 import type { StateDAO } from "../daos/StateDAO";
 import type { MunicipalityDAO } from "../daos/MunicipalityDAO";
 import type { ParishDAO } from "../daos/ParishDAO";
@@ -21,6 +21,82 @@ import {
     HOUSING_URL
 } from "./apiUrl";
 
+async function fetchDaosToApplicantModel(applicantDao: ApplicantDAO): Promise<ApplicantModel> {
+    let stateDao: StateDAO | undefined;
+    let municipalityDao: MunicipalityDAO | undefined;
+    let parishDao: ParishDAO | undefined;
+    let headEducationDao: EducationLevelDAO | undefined;
+    let applicantEducationDao: EducationLevelDAO | undefined;
+    let workConditionDao: WorkConditionDAO | undefined;
+    let activityConditionDao: ActivityConditionDAO | undefined;
+
+    if (applicantDao.idState){
+        const stateResponse = await fetch(`${STATE_URL}/${applicantDao.idState}`);
+        const stateData = await stateResponse.json();
+        stateDao = stateData.data;
+    }
+
+    if (applicantDao.municipalityNumber){
+        const municipalityResponse = await fetch(`${MUNICIPALITY_URL}/${applicantDao.municipalityNumber}`);
+        const municipalityData = await municipalityResponse.json();
+        municipalityDao = municipalityData.data;
+    }
+
+    if (applicantDao.parishNumber){
+        const parishResponse = await fetch(`${PARISH_URL}/${applicantDao.parishNumber}`);
+        const parishData = await parishResponse.json();
+        parishDao = parishData.data;
+    }
+
+    if (applicantDao.headEducationLevelId){
+        const headEducationResponse = await fetch(`${EDUCATION_LEVEL_URL}/${applicantDao.headEducationLevelId}`);
+        const headEducationData = await headEducationResponse.json();
+        headEducationDao = headEducationData.data;
+    }
+
+    if (applicantDao.applicantEducationLevelId){
+        const applicantEducationResponse = await fetch(`${EDUCATION_LEVEL_URL}/${applicantDao.applicantEducationLevelId}`);
+        const applicantEducationData = await applicantEducationResponse.json();
+        applicantEducationDao = applicantEducationData.data;
+    }
+
+    if (applicantDao.workConditionId){
+        const workConditionResponse = await fetch(`${WORK_CONDITION_URL}/${applicantDao.workConditionId}`);
+        const workConditionData = await workConditionResponse.json();
+        workConditionDao = workConditionData.data;
+    }
+
+    if (applicantDao.activityConditionId){
+        const activityConditionResponse = await fetch(`${ACTIVITY_CONDITION_URL}/${applicantDao.activityConditionId}`);
+        const activityConditionData = await activityConditionResponse.json();
+        activityConditionDao = activityConditionData.data;
+    }
+
+    // Fetch Family Home
+    const familyHomeResponse = await fetch(`${FAMILY_HOME_URL}/${applicantDao.identityCard}`);
+    const familyHomeData = await familyHomeResponse.json();
+    const familyHomeDao: FamilyHomeDAO = familyHomeData.data;
+
+    // Fetch Housing
+    const housingResponse = await fetch(`${HOUSING_URL}/${applicantDao.identityCard}`);
+    const housingData = await housingResponse.json();
+    const housingDao: HousingDAO = housingData.data;
+
+
+    return daoToApplicantModel(
+        applicantDao,
+        familyHomeDao,
+        housingDao,
+        headEducationDao,
+        applicantEducationDao,
+        workConditionDao,
+        activityConditionDao,
+        stateDao,
+        municipalityDao,
+        parishDao,
+    );
+}
+
 export function getApplicantRepository(): ApplicantRepository {
     return {
         findAllApplicants: async () => {
@@ -30,63 +106,7 @@ export function getApplicantRepository(): ApplicantRepository {
 
             const applicantsWithAllData = await Promise.all(
                 applicantDAOs.map(async (applicantDao) => {
-                    // Fetch State
-                    const stateResponse = await fetch(`${STATE_URL}/${applicantDao.idState}`);
-                    const stateData = await stateResponse.json();
-                    const stateDao: StateDAO = stateData.data;
-
-                    // Fetch Municipality
-                    const municipalityResponse = await fetch(`${MUNICIPALITY_URL}/${applicantDao.municipalityNumber}`);
-                    const municipalityData = await municipalityResponse.json();
-                    const municipalityDao: MunicipalityDAO = municipalityData.data;
-
-                    // Fetch Parish
-                    const parishResponse = await fetch(`${PARISH_URL}/${applicantDao.parishNumber}`);
-                    const parishData = await parishResponse.json();
-                    const parishDao: ParishDAO = parishData.data;
-
-                    // Fetch Head Education Level
-                    const headEducationResponse = await fetch(`${EDUCATION_LEVEL_URL}/${applicantDao.headEducationLevelId}`);
-                    const headEducationData = await headEducationResponse.json();
-                    const headEducationDao: EducationLevelDAO = headEducationData.data;
-
-                    // Fetch Applicant Education Level
-                    const applicantEducationResponse = await fetch(`${EDUCATION_LEVEL_URL}/${applicantDao.applicantEducationLevelId}`);
-                    const applicantEducationData = await applicantEducationResponse.json();
-                    const applicantEducationDao: EducationLevelDAO = applicantEducationData.data;
-
-                    // Fetch Work Condition
-                    const workConditionResponse = await fetch(`${WORK_CONDITION_URL}/${applicantDao.workConditionId}`);
-                    const workConditionData = await workConditionResponse.json();
-                    const workConditionDao: WorkConditionDAO = workConditionData.data;
-
-                    // Fetch Activity Condition
-                    const activityConditionResponse = await fetch(`${ACTIVITY_CONDITION_URL}/${applicantDao.activityConditionId}`);
-                    const activityConditionData = await activityConditionResponse.json();
-                    const activityConditionDao: ActivityConditionDAO = activityConditionData.data;
-
-                    // Fetch Family Home
-                    const familyHomeResponse = await fetch(`${FAMILY_HOME_URL}/${applicantDao.identityCard}`);
-                    const familyHomeData = await familyHomeResponse.json();
-                    const familyHomeDao: FamilyHomeDAO = familyHomeData.data;
-
-                    // Fetch Housing
-                    const housingResponse = await fetch(`${HOUSING_URL}/${applicantDao.identityCard}`);
-                    const housingData = await housingResponse.json();
-                    const housingDao: HousingDAO = housingData.data;
-
-                    return daoToApplicantModel(
-                        applicantDao,
-                        stateDao,
-                        municipalityDao,
-                        parishDao,
-                        headEducationDao,
-                        applicantEducationDao,
-                        workConditionDao,
-                        activityConditionDao,
-                        familyHomeDao,
-                        housingDao
-                    );
+                    return await fetchDaosToApplicantModel(applicantDao);
                 })
             );
 
@@ -99,63 +119,7 @@ export function getApplicantRepository(): ApplicantRepository {
             const applicantData = await response.json();
             const applicantDao: ApplicantDAO = applicantData.data;
 
-            // Fetch State
-            const stateResponse = await fetch(`${STATE_URL}/${applicantDao.idState}`);
-            const stateData = await stateResponse.json();
-            const stateDao: StateDAO = stateData.data;
-
-            // Fetch Municipality
-            const municipalityResponse = await fetch(`${MUNICIPALITY_URL}/${applicantDao.municipalityNumber}`);
-            const municipalityData = await municipalityResponse.json();
-            const municipalityDao: MunicipalityDAO = municipalityData.data;
-
-            // Fetch Parish
-            const parishResponse = await fetch(`${PARISH_URL}/${applicantDao.parishNumber}`);
-            const parishData = await parishResponse.json();
-            const parishDao: ParishDAO = parishData.data;
-
-            // Fetch Head Education Level
-            const headEducationResponse = await fetch(`${EDUCATION_LEVEL_URL}/${applicantDao.headEducationLevelId}`);
-            const headEducationData = await headEducationResponse.json();
-            const headEducationDao: EducationLevelDAO = headEducationData.data;
-
-            // Fetch Applicant Education Level
-            const applicantEducationResponse = await fetch(`${EDUCATION_LEVEL_URL}/${applicantDao.applicantEducationLevelId}`);
-            const applicantEducationData = await applicantEducationResponse.json();
-            const applicantEducationDao: EducationLevelDAO = applicantEducationData.data;
-
-            // Fetch Work Condition
-            const workConditionResponse = await fetch(`${WORK_CONDITION_URL}/${applicantDao.workConditionId}`);
-            const workConditionData = await workConditionResponse.json();
-            const workConditionDao: WorkConditionDAO = workConditionData.data;
-
-            // Fetch Activity Condition
-            const activityConditionResponse = await fetch(`${ACTIVITY_CONDITION_URL}/${applicantDao.activityConditionId}`);
-            const activityConditionData = await activityConditionResponse.json();
-            const activityConditionDao: ActivityConditionDAO = activityConditionData.data;
-
-            // Fetch Family Home
-            const familyHomeResponse = await fetch(`${FAMILY_HOME_URL}/${applicantDao.identityCard}`);
-            const familyHomeData = await familyHomeResponse.json();
-            const familyHomeDao: FamilyHomeDAO = familyHomeData.data;
-
-            // Fetch Housing
-            const housingResponse = await fetch(`${HOUSING_URL}/${applicantDao.identityCard}`);
-            const housingData = await housingResponse.json();
-            const housingDao: HousingDAO = housingData.data;
-
-            return daoToApplicantModel(
-                applicantDao,
-                stateDao,
-                municipalityDao,
-                parishDao,
-                headEducationDao,
-                applicantEducationDao,
-                workConditionDao,
-                activityConditionDao,
-                familyHomeDao,
-                housingDao
-            );
+            return await fetchDaosToApplicantModel(applicantDao);
         },
 
         createApplicant: async (data) => {
