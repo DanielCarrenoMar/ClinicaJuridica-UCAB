a267649da22bc748e49587f761e18310a571591f
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-12-31 11:37:17 -0400

    refactaor: ya no se vuelve a crear el obj prisma en index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,8 @@
-import { PrismaClient } from '#src/generated/client.js';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 import express from 'express';
 import cors from 'cors';
 
 import apiRoutes from './api/v1/routes/index.js';
 
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
 const app = express();
 const port = process.env.PORT || 3000;
 
