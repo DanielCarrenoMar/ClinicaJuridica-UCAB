a20a0c378dfe73f33ddb4711a0bcea9af8d478cd
Daniel <dacarreno.24@est.ucab.edu.ve>
2025-12-31 12:11:39 -0400

    refactor: interfaces de repo

--- frontEnd/src/domain/repositories.ts
+++ frontEnd/src/domain/repositories.ts
@@ -1,7 +1,13 @@
+import type { CaseActionDAO } from "#database/daos/caseActionDAO.ts";
 import type { CaseDAO } from "#database/daos/caseDAO.ts";
+import type { CaseStatusDAO } from "#database/daos/caseStatusDAO.ts";
 import type { ApplicantModel } from "./models/applicant"
 import type { BeneficiaryModel } from "./models/beneficiary";
 import type { CaseModel } from "./models/case";
+import type { CaseActionModel } from "./models/caseAction";
+import type { CaseStatusModel } from "./models/caseStatus";
+import type { StatusCaseAmountModel } from "./models/statusCaseAmount";
+import type { StudentModel } from "./models/student";
 
 export interface BeneficiaryRepository {
     findAllBeneficiaries(): Promise<BeneficiaryModel[]>;
@@ -21,7 +27,30 @@ export interface ApplicantRepository {
 export interface CaseRepository {
     findAllCases(): Promise<CaseModel[]>;
     findCaseById(id: string): Promise<CaseModel | null>;
+    findBeneficiariesByCaseId(idCase: number): Promise<BeneficiaryModel[]>;
+    findCaseStatusByCaseId(idCase: number): Promise<CaseStatusModel[]>;
+    findStudentsByCaseId(idCase: number): Promise<StudentModel[]>;
+    getStatusCaseAmount(): Promise<StatusCaseAmountModel[]>;
     createCase(data: CaseDAO): Promise<CaseDAO>;
+    createCaseStatusFromCaseId(data: CaseStatusDAO): Promise<CaseStatusDAO>;
     updateCase(id: string, data: Partial<CaseModel>): Promise<CaseModel>;
     deleteCase(id: string): Promise<void>;
+}
+
+export interface CaseActionRepository {
+    findAllCaseActions(): Promise<CaseActionModel[]>;
+    findCaseActionById(id: string): Promise<CaseActionModel | null>;
+    createCaseAction(data: CaseActionDAO): Promise<CaseActionDAO>;
+}
+
+export interface UserRepository {
+    findUserById(id: string): Promise<any | null>;
+}
+
+export interface StudentRepository {
+    findStudentById(id: string): Promise<any | null>;
+}
+
+export interface TeacherRepository {
+    findTeacherById(id: string): Promise<any | null>;
 }
