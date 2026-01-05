c29a2c9db3eeccab4200914b0d99cddae70172ba
Daniel <dacarreno.24@est.ucab.edu.ve>
2026-01-05 16:56:31 -0400

    feat: poder cambiar status de caso

--- frontEnd/src/data/database/daos/caseStatusDAO.ts
+++ frontEnd/src/data/database/daos/caseStatusDAO.ts
@@ -1,10 +1,7 @@
 import type { CaseStatusTypeDAO } from "../typesDAO.ts";
 
 export interface CaseStatusDAO {
-  idCase: number;
-  statusNumber: number;
   status: CaseStatusTypeDAO;
   reason?: string;
   userId: string;
-  registryDate: Date;
 }
--- frontEnd/src/data/database/daos/caseStatusInfoDAO.ts
+++ frontEnd/src/data/database/daos/caseStatusInfoDAO.ts
@@ -1,6 +1,9 @@
 import type { CaseStatusDAO } from "./caseStatusDAO.ts";
 
 export interface CaseStatusInfoDAO extends CaseStatusDAO {
+  idCase: number;
+  statusNumber: number;
+  registryDate: Date;
   userName: string;
   caseCompoundKey: string;
 }
