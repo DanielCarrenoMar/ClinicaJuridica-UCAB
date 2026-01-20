a703b9fd81ea0a1eccbfcf3ad29301be448f233f
DanielCarrenoMar <dacarreno.24@est.ucab.edu.ve>
2026-01-20 16:42:35 -0400

    refactor: tipos y funcion de dao a model para los model de reportes

--- frontEnd/src/domain/models/reports/beneficiaryTypeDistribution.ts
+++ frontEnd/src/domain/models/reports/beneficiaryTypeDistribution.ts
@@ -1,4 +1,15 @@
+import type { CaseBeneficiaryTypeModel } from "#domain/typesModel.ts";
+import { typeDaoToCaseBeneficiaryTypeModel } from "#domain/typesModel.ts";
+import type { BeneficiaryTypeDistribution } from "#database/daos/reports/beneficiaryTypeDistributionDAO.ts";
+
 export interface BeneficiaryTypeDistributionModel {
-  type: string;
+  type: CaseBeneficiaryTypeModel;
   count: number;
+}
+
+export function beneficiaryTypeDistributionDAOToModel(dao: BeneficiaryTypeDistribution): BeneficiaryTypeDistributionModel {
+  return {
+    type: typeDaoToCaseBeneficiaryTypeModel(dao.type),
+    count: dao.count,
+  };
 }
--- frontEnd/src/domain/models/reports/parishDistribution.ts
+++ frontEnd/src/domain/models/reports/parishDistribution.ts
@@ -1,5 +1,16 @@
+import type { BeneficiaryTypeModel } from "#domain/typesModel.ts";
+import type { ParishDistribution } from "#database/daos/reports/parishDistributionDAO.ts";
+
 export interface ParishDistributionModel {
-  type: 'Applicants' | 'Beneficiaries';
+  type: BeneficiaryTypeModel;
   parish: string;
   count: number;
+}
+
+export function parishDistributionDAOToModel(dao: ParishDistribution): ParishDistributionModel {
+  return {
+    type: dao.type === 'Applicants' ? 'Solicitante' : 'Beneficiario',
+    parish: dao.parish,
+    count: dao.count,
+  };
 }
